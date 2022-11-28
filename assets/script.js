d43157b6e98bca6a48fca65cd04b8ba2089132c1
Warfa <warfahassan35@yahoo.com>
2022-11-27 23:06:45 -0500

    Added the current time and set interval

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,21 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// Displays current Day and Time
+var timeDisplayEl = $('#currentDay');
+
+// handle displaying the time
+function displayTime() {
+  var rightNow = dayjs().format('dddd, MMMM D, YYYY h:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+// Calls the function for the time and date
+// Set an interval for the time to update every second
+displayTime();
+setInterval(displayTime, 1000);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
